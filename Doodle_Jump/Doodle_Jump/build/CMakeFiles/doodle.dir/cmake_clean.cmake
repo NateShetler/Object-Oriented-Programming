2784cbd5e3fb26cef187cced321edfd0ec1e6ee2
NateShetler <nate.shetler@gmail.com>
2019-02-01 20:23:39 -0500

    Initial Doodle Jump Program

---
+++ Doodle_Jump/Doodle_Jump/build/CMakeFiles/doodle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doodle.dir/main.cpp.o"
+  "doodle.pdb"
+  "doodle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/doodle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
